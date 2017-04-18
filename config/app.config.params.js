57bee2623aa77da1692cb02d1f60445eac239a46
Priyank <p4priyank@github.com>
2017-04-18 20:01:39 +0530

    Initialize repo

---
+++ config/app.config.params.js
@@ -0,0 +1,9 @@
+module.exports = {
+  LIST_SWAGGER_CONF_ENDPOINTS: [{
+    'className': 'SwaggerTestApi',
+    'apiName': 'TEST',
+    'url': '', // host url for ex : http://192.168.0.1
+    'path': '../src/common/swagger-providers/test-api.provider.ts',
+    'sub_path': '/api/doc?format=openapi'
+  }]
+};
